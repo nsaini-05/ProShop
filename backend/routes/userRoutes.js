e2e84e9131c0691b345588f2630ea588ee2fc63b
Neeraj <asaini7788@gmail.com>
2021-09-25 01:21:52 -0600

    Password Encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { authUser, getUserProfile } from "../controllers/userControllers.js"
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from "../controllers/userControllers.js"
 import { protect } from "../middlewares/authMiddleware.js"
 const router = express.Router()
 router.post("/login", authUser)
 router.get("/profile", protect, getUserProfile)
+router.post("/", registerUser)
 
 export default router

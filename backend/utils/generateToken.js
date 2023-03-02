0b0fcbd6218d95b7fd9f2839fce4072f9cfa85ea
Neeraj <asaini7788@gmail.com>
2023-03-01 22:40:17 -0700

    Update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,10 @@
 import jwt from "jsonwebtoken"
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_TOKEN_SECRET, {
+
+  console.log(process.env.JWT_TOKEN_SECRET)
+  const secret = process.env.JWT_TOKEN_SECRET;
+  return jwt.sign({ id }, secret, {
     expiresIn: "30d",
   })
 }
